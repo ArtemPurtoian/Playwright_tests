4a2015d8e8500782d7e86bf7cc30e690165f9ff1
ArtemPurtoian <artemios.od@gmail.com>
2025-09-04 18:56:56 +0300

    Added POM classes

---
+++ page_object_models/base.py
@@ -0,0 +1,25 @@
+from playwright.sync_api import expect
+
+
+"""
+Creating the Base class that implements Playwright actions.
+"""
+class Base:
+    def __init__(self, page):
+        self.page = page
+
+    def has_url(self, url):
+        expect(self.page).to_have_url(url)
+        return self
+
+    def is_visible(self, locator):
+        expect(self.page.locator(locator)).to_be_visible()
+        return self
+
+    def fill_input(self, locator, input_value):
+        self.page.locator(locator).fill(input_value)
+        return self
+
+    def click_element(self, locator):
+        self.page.locator(locator).click()
+        return self
