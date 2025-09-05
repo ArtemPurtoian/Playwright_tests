99566edd1bd2528b0322193bdb01a423ba51f6f9
ArtemPurtoian <artemios.od@gmail.com>
2025-09-05 14:06:13 +0300

    Added get_user_credentials() function

--- utils/env_reader.py
+++ utils/env_reader.py
@@ -11,3 +11,7 @@ def get_url():
 def get_browser():
     browser = os.getenv("BROWSER")  # chromium/firefox/webkit
     return browser
+
+def get_user_credentials():
+    username, password = os.getenv("USER"), os.getenv("PASSWORD")
+    return username, password
