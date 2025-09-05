8620174424591c861a6e05aae6c44c7219b4beb2
ArtemPurtoian <artemios.od@gmail.com>
2025-09-05 14:16:38 +0300

    Removed user creds from test_login_functionality() script

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -6,5 +6,4 @@ def test_login_page_elements(open_page):
     Login(open_page).has_url(get_url()).check_page_elements()
 
 def test_login_functionality(open_page):
-    Login(open_page).login("standard_user",
-                           "secret_sauce").check_login_success()
+    Login(open_page).login().check_login_success()
