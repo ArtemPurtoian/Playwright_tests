432cf35e4ab149b29dbf81c657cf286303d37c9e
ArtemPurtoian <artemios.od@gmail.com>
2025-09-05 13:27:41 +0300

    Added test_inventory_page.py

---
+++ tests/test_inventory_page.py
@@ -0,0 +1,8 @@
+from page_object_models.inventory import Inventory
+from page_object_models.login import Login
+
+
+def test_inventory_page_elements(open_page):
+    Login(open_page).login("standard_user",
+                           "secret_sauce")
+    Inventory(open_page).check_inventory_elements()
